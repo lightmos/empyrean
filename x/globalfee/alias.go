d14d662c03e163c1608afb91c431d4073a278c97
wen.lang <llfcjyy@outlook.com>
2023-08-29 19:28:08 +0800

    feat: init repo, create chain project by modules of globalfee and restaking

---
+++ x/globalfee/alias.go
@@ -0,0 +1,10 @@
+package globalfee
+
+import (
+	"github.com/lightmos/empyrean/x/globalfee/types"
+)
+
+const (
+	ModuleName = types.ModuleName
+	StoreKey   = types.StoreKey
+)
---
+++ x/globalfee/types/keys.go
@@ -0,0 +1,11 @@
+package types
+
+const (
+	// ModuleName is the name of the this module
+	ModuleName = "globalfee"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	QuerierRoute = ModuleName
+)
